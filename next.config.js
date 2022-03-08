a4c4b9cc2e1d9c58de836ce2447125b9111c1d5e
igor silva <igorsilva@igors-Mac-mini.local>
2022-03-08 17:10:45 +0000

    make it better

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-public:['./fonts']
-}
+  async headers() {
+    return [
+      {
+        source: "/fonts/IntegralCF-Bold.otf",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
+
