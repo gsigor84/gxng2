66babfb8391fd2f958ed61a6d65ceaf48aa2853b
igor silva <igorsilva@igors-Mac-mini.local>
2022-03-12 06:06:35 +0000

    make it better

--- pages/strength/chest.js
+++ pages/strength/chest.js
@@ -3,27 +3,14 @@ import {API_URL} from '@/config/index'
 
 import Exe from 'components/ExeModule/LayoutModule'
 
-const chest = ({chests}) => {
+const chest = () => {
   return (
 
 <>
-<Exe chests={chests}  />
+<Exe />
 </>
   )
 }
 
-export async function getStaticProps() {
-    const res = await fetch(`${API_URL}/chests`)
-    const chests = await res.json()
-
-  
-
-    return{
-        props: {chests:chests.slice(-1)},
-     
-     
-    }
-
-}
 
 export default chest
